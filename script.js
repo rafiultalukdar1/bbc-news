43ab1897801ef4ad51d8dbbe12cd54c4390c4458
rafi0012 <rafiultalukdar007@gmail.com>
2025-09-13 13:13:41 +0600

    Added Modal

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const categorieContainer = document.getElementById("categorieContainer");
 const newsContainer = document.getElementById("news-container");
 const bookmarkContainer = document.getElementById("bookmark-container");
-
-
+const bookmarkCount = document.getElementById("bookmark-count");
+const modalContainer = document.getElementById("modal-container");
+const modalBox = document.getElementById("modal_box");
 
 
 const loadCategory = () => {
@@ -20,8 +21,7 @@ const loadCategory = () => {
 const showCategory = (categories) => {
     categories.forEach(category => {
         categorieContainer.innerHTML += `
-            <button id="${category.id}" class="text-[#141414] text-[18px] font-semibold cursor-pointer hover:border-b-[3px] hover:border-[#141414] duration-100">${category.title}</button>
-        `;
+            <button id="${category.id}" class="text-[#141414] text-[18px] font-semibold cursor-pointer hover:border-b-[3px] hover:border-[#141414] duration-100">${category.title}</button>`;
     });
 
     categorieContainer.addEventListener('click', (e) => {
@@ -52,15 +52,15 @@ const showNews = (articles) => {
     newsContainer.innerHTML = "";
     articles.forEach((artic) => {
         newsContainer.innerHTML += `
-        <div class="borde border-gray-300 rounded-lg">
+        <div class="borde border-gray-300 rounded-lg shadow-[0px_2px_8px_0px_rgba(99,99,99,0.2)] overflow-hidden">
             <div>
                 <img class="w-full" src="${artic.image.srcset[5].url}" />
             </div>
             <div id="${artic.id}" class="p-2">
                 <h1 class="font-bold text-[18px]">${artic.title}</h1>
                 <p class="text-[16px] py-1 mb-[8px]">${artic.time}</p>
-                <button class="py-[8px] px-[12px] cursor-pointer border border-gray-300 rounded-md">Bookmark</button>
-                <button class="py-[8px] px-[12px] cursor-pointer border border-gray-300 rounded-md">View Details</button>
+                <button class="text-[14px] py-[8px] px-[10px] cursor-pointer border border-gray-300 rounded-md">Bookmark</button>
+                <button class="text-[14px] py-[8px] px-[10px] cursor-pointer border border-gray-300 rounded-md">View Details</button>
             </div>
         </div>`;
     })
@@ -74,6 +74,10 @@ newsContainer.addEventListener('click', (e) => {
     if(e.target.innerText === 'Bookmark') {
         handleBookmark(e);
     }
+
+    if(e.target.innerText === 'View Details') {
+        viewModal(e);
+    }
     
 })
 
@@ -84,7 +88,7 @@ const handleBookmark = (e) => {
         id: newsId,
         title: newsTitle
     });
-    showBookmarks(bookmarks)
+    showBookmarks(bookmarks);
 }
 
 const showBookmarks = (bookmarks) => {
@@ -96,6 +100,7 @@ const showBookmarks = (bookmarks) => {
             <button onclick="deleteBookmark('${bookmark.id}')" class="py-[3px] px-[8px] border border-gray-300 rounded-md text-[14px] font-[500] cursor-pointer mt-[5px] ">Delete</button>
         </div>`;
     });
+    bookmarkCount.innerText = bookmarks.length;
 };
 
 const deleteBookmark = (bookmarkId) => {
@@ -103,6 +108,33 @@ const deleteBookmark = (bookmarkId) => {
     bookmarks = filterBookmark
     showBookmarks(bookmarks)
 }
+
+const viewModal = (e) => {
+    const newsId = e.target.parentNode.id;
+    fetch(`https://news-api-fs.vercel.app/api/news/${newsId}`)
+    .then(res => res.json())
+    .then(data => {
+        showModalData(data.article)
+    })
+}
+
+
+const showModalData = (article) => {
+    modalBox.showModal()
+    modalContainer.innerHTML = `
+    <div>
+        <img class="w-full" src="${article.images[5].url}" />
+        <h2 class="font-bold text-[18px] py-3">${article.title}</h2>
+        <p>${article.content.join("")}</p>
+    </div>`;
+}
+
+
+
+
+
+
+
 
 
 
